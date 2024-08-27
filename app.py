1fb4b6b034065cb0b4d82bc7eb9f5b092e46b82b
P-ASilva <pedroas5.al.insper.edu>
2024-08-27 15:57:45 -0300

    feat: simple-crawler implemented in route /request

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
-import os
-import re
 from flask import Flask
+from flask import request
 from bs4 import BeautifulSoup
-from datetime import datetime
 import requests
+import pandas as pd
 
 app = Flask(__name__)
 
@@ -11,50 +10,54 @@ app = Flask(__name__)
 def hello_world():
     return "<p>Hello, World!</p>"
 
+
 @app.route("/request")
 def request_test():
-    api_endpoint = "https://www.cnnbrasil.com.br/economia/macroeconomia/"
-    response = requests.get(api_endpoint)
+    base_api_endpoint = "https://www.cnnbrasil.com.br/economia/macroeconomia/pagina/"
+    # create a df with columns 'title', 'subtitle', 'content'
+    df = pd.DataFrame(columns = ['title', 'subtitle', 'content'])
+    # loop
+    c = 0
+    for pag in range(1, 100):
+        api_endpoint = f"{base_api_endpoint}{pag}/"
+        response = requests.get(api_endpoint)
 
-    if response.status_code == 200:
-        # Process the response data
-        html_data = response.text
-        soup = BeautifulSoup(html_data, 'html.parser')
-
-        fine_soup = soup.find_all('li', class_ = 'home__list__item')
-        # print(fine_soup)
-        for li in fine_soup:
-            print(li.find('a').get('href'))
-            datetime_str = li.find('span').get_text()
-            print(datetime_str)
-            # filter out the links whose last date are past the latest analysed date
-            latest_date = os.getenv('LATEST_SCRAPE_DATE')
-            # YY/MM/DD HH:MM
-            print("latest_date")
-            latest_date = datetime.strptime(latest_date, "%Y-%m-%d %H:%M")
-            # use re lib to filter date from datetime
-            print("current_date")
-            date_regex = r'\d{2}[\/\-]\d{2}[\/\-]\d{4}'
-            date = re.findall(date_regex, datetime_str)[0]
-            print(date)
-            time_regex = r'\d{2}:\d{2}'
-            time = re.findall(time_regex, datetime_str)[0]
-            print(time)
-            print('time?')
-            dateandtime = date + " " + time
-
-            date = datetime.strptime(dateandtime, "%d/%m/%Y %H:%M")
-
-            if date >= latest_date:
-                # use re lib to filter time from datetime
-                print("Date is newer than latest date")
-            else:
-                print("Date is older than latest date")
-
-            # pegar info do link e adicionar como uma linha do csv
-
-    else:
-        print(f"Error: {response.status_code}")
-        return "HELP"
-
+        if response.status_code == 200:
+            # Process the response data
+            html_data = response.text
+            soup = BeautifulSoup(html_data, 'html.parser')
+            fine_soup = soup.find_all('li', class_ = 'home__list__item')
+            for obj in fine_soup:
+                # extract link
+                link = obj.find('a')['href']
+                follow_up = requests.get(link)
+                follow_soup = BeautifulSoup(follow_up.text, 'html.parser')
+                # isolate tag <article>
+                article = follow_soup.find('article')
+                # extract the text from the header class 'single-header'
+                header = article.find('header', class_ = 'single-header')
+                # find title with tag <h1> in header
+                title = header.find('h1').text
+                print(title)
+                # find subtitle with tag <p> in header with class 'single-header__excerpt'
+                subtitle = header.find('p', class_ = 'single-header__excerpt').text
+                # extract all with tag <p> in div with class 'single-content'
+                content = article.find('div', class_ = 'single-content').find_all('p')
+                # join all <p> in a single string removing the tag
+                content = ' '.join([c.text for c in content])
+                # add a new row to the df with the extracted data
+                df.loc[c] = {'title': title, 'subtitle': subtitle, 'content': content}
+                c += 1
+        # save df to csv
+        df.to_csv('cnn.csv')
+    
     return f"{fine_soup}"
+
+# make a route named query, that takes as a now url param a string named q 
+
+@app.route("/query")
+def query():
+    q = request.args.get('query')
+
+
+    return f"Query: {q}"
